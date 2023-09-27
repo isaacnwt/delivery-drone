cce50a9578026ffa1f9a7f109425efc61d093dfc
isaacnwt <isaacnewton02@hotmail.com>
2023-09-26 22:14:03 -0300

    feat: Main implement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Drone drone = new Drone();
+        int A, B, C, H, W;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        H = sc.nextInt();
+        W = sc.nextInt();
+
+        Box box = new Box(A, B, C);
+        drone.setBox(box);
+
+        Window window = new Window(H, W);
+
+        System.out.println(drone.deliver(window));
     }
 }
